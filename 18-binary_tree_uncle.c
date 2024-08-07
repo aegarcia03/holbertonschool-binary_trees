e6489aa9db9a89a68e73cf6fa46d8648d0a6dbb7
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-07 07:44:59 +0000

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,17 +11,15 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent;
-
 	if (node == NULL ||
 		node->parent == NULL ||
 		node->parent->parent == NULL)
-	       return (NULL);
+	{
+		return (NULL);
+	}
 
-	grandparent = node->parent->parent;
-
-	if (grandparent->left == node->parent)
-		return (grandparent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 	else
-		return (grandparent->left);
+		return (node->parent->parent->left);
 }
