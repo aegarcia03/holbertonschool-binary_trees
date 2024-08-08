cf44b92a5e1085d7a6a331de381613755075cc5c
David Zajicek <david.a.zajicek@outlook.com>
2024-08-08 02:38:49 +0000

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measure depth
+ * @tree: node to measure
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+	binary_tree_t *node;
+
+	if (tree == NULL)
+		return (depth);
+	node = tree;
+	while (node->parent != NULL)
+	{
+		depth++;
+		node = node->parent;
+	}
+	return (depth);
+}
