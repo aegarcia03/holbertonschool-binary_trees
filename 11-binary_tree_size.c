c12db8126a77d36be8b599fa0b8cc97fafded471
David Zajicek <david.a.zajicek@outlook.com>
2024-08-08 01:22:48 +0000

    why am I using a pointer?

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,16 +3,15 @@
 /**
  * recursive_count - count nodes recursively
  * @node: node to count
- * @count: address of the counter
  * Return: void
  */
-void recursive_count(const binary_tree_t *node, size_t *count)
+size_t recursive_count(const binary_tree_t *node)
 {
 	if (node->left != NULL)
 		recursive_count(node->left, count);
 	if (node->right != NULL)
 		recursive_count(node->right, count);
-	count = count + 1;
+	return (1);
 }
 
 /**
@@ -26,6 +25,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (size);
-	recursive_count(tree, &size);
+	size = recursive_count(tree);
 	return (size);
 }
